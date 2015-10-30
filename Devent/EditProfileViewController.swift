aec85863a07b1c644f7d5b04245c2d7775f3582d
Can Ceran <can@ceran.email>
2015-10-30 21:17:09 +0300

    hadi bismillah

--- Devent/EditProfileViewController.swift
+++ Devent/EditProfileViewController.swift
@@ -4,67 +4,13 @@ import UIKit
 
 class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    // MARK: PROPOERTIES
-
-
-    
-    // MARK: ACTIONS
-    
-    //add choose birthday function
-    
+/*
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-<<<<<<< HEAD
 
-//    
-//
-//    @IBAction func chooseProfilePictureButton(sender: AnyObject) {
-//        
-//        var myPickerController = UIImagePickerController()
-//        myPickerController.delegate = self
-//        myPickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-//        
-//        self.presentViewController(myPickerController, animated: true, completion: nil)
-//        
-//    }
-//    
-//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-//        profilePicture.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-//        self.dismissViewControllerAnimated(true, completion: nil)
-//    }
-//    
-//   
-//    @IBAction func saveButton(sender: AnyObject) {
-//        
-//        let myUser = PFUser.currentUser()!
-//        let profileImagedata = UIImageJPEGRepresentation(profilePicture.image!, 1)
-//        
-//        let userFirstName = firstNameTextField.text
-//        let userLastName = lastNameTextField.text
-//        let userAge = ageLabel.text
-//        
-//        myUser.setObject(userFirstName!, forKey: "firstName")
-//        myUser.setObject(userLastName!, forKey: "lastName")
-//        myUser.setObject(userAge!, forKey: "userAge")
-//        
-//         if(profileImagedata != nil) {
-//            let profileFileObject = PFFile(data:profileImagedata!)
-//            myUser.setObject(profileFileObject!, forKey: "profilePicture")
-//        
-//        }
-//        
-//        myUser.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-//        
-//        }
-//        
-//        self.performSegueWithIdentifier("eventsSeque", sender: self)
-//        
-//    }
-//
-
-=======
+*/
 /*
     
 
@@ -112,7 +58,7 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
     }
 
 */
->>>>>>> master
+
 
     
 }
