cc8b4a467c303c2d950ac57182c4afbae3ebe778
Can Ceran <can@ceran.email>
2015-10-30 11:54:27 +0300

    ProfileAsTableView WIP (30 Oct)

--- Devent/EditProfileViewController.swift
+++ Devent/EditProfileViewController.swift
@@ -17,51 +17,51 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         super.viewDidLoad()
     }
 
-    
-
-    @IBAction func chooseProfilePictureButton(sender: AnyObject) {
-        
-        var myPickerController = UIImagePickerController()
-        myPickerController.delegate = self
-        myPickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        
-        self.presentViewController(myPickerController, animated: true, completion: nil)
-        
-    }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        profilePicture.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-   
-    @IBAction func saveButton(sender: AnyObject) {
-        
-        let myUser = PFUser.currentUser()!
-        let profileImagedata = UIImageJPEGRepresentation(profilePicture.image!, 1)
-        
-        let userFirstName = firstNameTextField.text
-        let userLastName = lastNameTextField.text
-        let userAge = ageLabel.text
-        
-        myUser.setObject(userFirstName!, forKey: "firstName")
-        myUser.setObject(userLastName!, forKey: "lastName")
-        myUser.setObject(userAge!, forKey: "userAge")
-        
-         if(profileImagedata != nil) {
-            let profileFileObject = PFFile(data:profileImagedata!)
-            myUser.setObject(profileFileObject!, forKey: "profilePicture")
-        
-        }
-        
-        myUser.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-        
-        }
-        
-        self.performSegueWithIdentifier("eventsSeque", sender: self)
-        
-    }
-
+//    
+//
+//    @IBAction func chooseProfilePictureButton(sender: AnyObject) {
+//        
+//        var myPickerController = UIImagePickerController()
+//        myPickerController.delegate = self
+//        myPickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+//        
+//        self.presentViewController(myPickerController, animated: true, completion: nil)
+//        
+//    }
+//    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+//        profilePicture.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+//        self.dismissViewControllerAnimated(true, completion: nil)
+//    }
+//    
+//   
+//    @IBAction func saveButton(sender: AnyObject) {
+//        
+//        let myUser = PFUser.currentUser()!
+//        let profileImagedata = UIImageJPEGRepresentation(profilePicture.image!, 1)
+//        
+//        let userFirstName = firstNameTextField.text
+//        let userLastName = lastNameTextField.text
+//        let userAge = ageLabel.text
+//        
+//        myUser.setObject(userFirstName!, forKey: "firstName")
+//        myUser.setObject(userLastName!, forKey: "lastName")
+//        myUser.setObject(userAge!, forKey: "userAge")
+//        
+//         if(profileImagedata != nil) {
+//            let profileFileObject = PFFile(data:profileImagedata!)
+//            myUser.setObject(profileFileObject!, forKey: "profilePicture")
+//        
+//        }
+//        
+//        myUser.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+//        
+//        }
+//        
+//        self.performSegueWithIdentifier("eventsSeque", sender: self)
+//        
+//    }
+//
 
 
     
---
+++ Devent/ProfilePictureTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ProfilePictureTableViewCell.swift
+//  Devent
+//
+//  Created by Can Ceran on 29/10/15.
+//  Copyright © 2015 ES. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePictureTableViewCell: UITableViewCell {
+    
+    // MARK: PROPOERTIES
+    
+    @IBOutlet weak var profilePictureImageView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
