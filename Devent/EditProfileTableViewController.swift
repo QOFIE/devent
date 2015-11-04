114a85b15e304aeb531f6f548ce32a637e2bcc79
Can Ceran <can@ceran.email>
2015-11-04 21:05:06 +0300

    deneme

--- Devent/EditProfileTableViewController.swift
+++ Devent/EditProfileTableViewController.swift
@@ -16,6 +16,7 @@ class EditProfileTableViewController: UITableViewController {
     
     // MARK: GENERAL ACTIONS
     
+    //yenilikleri unutma
     
     @IBAction func saveChanges(sender: UIBarButtonItem) {
         
