1eb3fd4560ebb82371613a3b2954dcbb0034ec85
Erman Sefer <sefer.erman@gmail.com>
2015-11-26 10:23:47 +0100

    eee

---
+++ Devent/ShowDetailDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ShowDetailDelegate.swift
+//  Devent
+//
+//  Created by Erman Sefer on 26/11/15.
+//  Copyright © 2015 ES. All rights reserved.
+//
+
+import Foundation
+
+protocol ShowDetailDelegate {
+    func showDetail(event: AnyObject)
+}
